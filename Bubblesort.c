7dfe0b4dd41be957bd47b71b9ecbcdf27536db08
Garam Choi <chlrkfka763@nate.com>
2016-08-02 17:04:53 +0900

    Bubblesort.c
    Simple example! so you have to reference this logic sentence and this is ascending order, if you want to make descending order, you can change this sentence from if (ary[j] > ary[j + 1]) to if (ary[j] < ary[j + 1]) except remaining sequence

---
+++ Bubblesort.c
@@ -0,0 +1,18 @@
+void Bubblesort(int ary[], int len)
+{
+	int i, j;
+	int temp;
+
+	for (i = 0; i < len - 1; i++)
+	{
+		for (j = 0; j < (len - i) - 1; j++)
+		{
+			if (ary[j] > ary[j + 1])
+			{
+				temp = ary[j];
+				ary[j] = ary[j + 1];
+				ary[j + 1] = temp;
+			}
+		}
+	}
+}
